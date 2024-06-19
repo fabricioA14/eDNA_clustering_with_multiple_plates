54a9d92afb905bbe29707deaaf912a7c08f352ba
Fabricio dos Anjos <73892283+fabricioA14@users.noreply.github.com>
2024-06-19 13:48:50 -0300

    Update 4_vsearch_unique.sh

--- 4_vsearch_unique.sh
+++ 4_vsearch_unique.sh
@@ -8,5 +8,5 @@ module load StdEnv VSEARCH/2.21.1-GCC-10.3.0
 # The output will be written to uniques_10.fasta, including sequences that appear at least 10 times (--minuniquesize 10).
 # The --sizeout option will include the size annotation in the output sequences, and --fasta_width 0 ensures no line wrapping in the output FASTA file.
 
-vsearch --derep_fulllength pooled_upper.fasta --output uniques_10.fasta --minuniquesize 10 --sizeout --fasta_width 0
+vsearch --derep_fulllength merged_file.fasta --output uniques_10.fasta --minuniquesize 10 --sizeout --fasta_width 0
 
